ca5fbaa29efd879909464ca605aa09ecfd7b5c54
Jaroslaw Charchula <charchula@gmail.com>
2022-06-27 19:01:48 +0200

    add localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 import { data } from "./data";
@@ -8,10 +8,16 @@ import {nanoid} from "nanoid";
 import "./style.css";
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    const [notes, setNotes] = useState(
+        () => JSON.parse(localStorage.getItem("notes")) || []
+    )    
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
     
     function createNewNote() {
         const newNote = {
@@ -19,7 +25,7 @@ export default function App() {
             body: "# Type your markdown note's title here"
         }
         setNotes(prevNotes => [newNote, ...prevNotes])
-        setCurrentNoteId(newNote.id)
+        setCurrentNoteId(newNote.id)        
     }
     
     function updateNote(text) {
@@ -27,7 +33,7 @@ export default function App() {
             return oldNote.id === currentNoteId
                 ? { ...oldNote, body: text }
                 : oldNote
-        }))
+        }))        
     }
     
     function findCurrentNote() {
